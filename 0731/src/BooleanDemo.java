4a8cda532a81fa07377b5b4a743ef416972bd8e2
tmznf963 <qhfhd963@naver.com>
2018-07-31 11:54:17 +0900

    update

---
+++ 0731/src/BooleanDemo.java
@@ -0,0 +1,11 @@
+
+public class BooleanDemo {
+
+	public static void main(String[] args) {
+		boolean flag = false;
+		
+		System.out.println("flag = "+flag);
+		System.out.printf("flag = %b",flag);//boolean Æ÷¸Ë %b
+	}
+
+}
---
+++ 0731/src/CharacterDemo.java
@@ -0,0 +1,9 @@
+
+public class CharacterDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
