4bc20622cb2628b09b6b04f27cbf84e926059b3e
tmznf963 <qhfhd963@naver.com>
2018-08-03 17:07:05 +0900

    update

--- 0803/src/EnCapDemo.java
+++ 0803/src/EnCapDemo.java
@@ -3,6 +3,12 @@ public class EnCapDemo {
 	public static void main(String[] args) {
 		Student std = new Student();
 		//std.name = "±èÃ¶¼ö";
+		std.setName("Ã¶¼öÅ´");
+		
 		//std.kor = 80;
+		std.setKor(80);
+		
+		System.out.println("name = "+std.getName());
+		System.out.println("name = "+std.getKor());
 	}
 }
--- 0803/src/Student.java
+++ 0803/src/Student.java
@@ -2,4 +2,18 @@
 public class Student {
 	private String name; //Encapsulation 은닉화
 	private int kor;
+	
+	public void setName(String irum) {
+		name = irum;;
+	}
+	public String getName() { //리턴타입 String
+		return name;
+	}
+	
+	public void setKor(int k) {
+		kor = k;
+	}
+	public int getKor() { //리턴타입 int
+		return kor;
+	}
 }
