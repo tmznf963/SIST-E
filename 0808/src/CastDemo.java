7e2459e71502d25f2a8e450606c97f23d45a6b18
tmznf963 <qhfhd963@naver.com>
2018-08-08 13:58:27 +0900

    update

---
+++ 0808/src/CastDemo.java
@@ -0,0 +1,29 @@
+/*
+ *형변환(Casting, Conversion)
+ *1. 자동형변환(암시적 형변환, implicit casting)
+ *		1)소 -> 대
+ *		2)3 + 3.14 = 6.14
+ *		3)"Hello" + 123 = "Hello123"
+ *2. 강제형변환(명시적 형변환, explicit casting)
+ * 	1)대 -> 소
+ * 	2)int su = (int)34L;
+ */
+public class CastDemo {
+	public static void main(String[] args) {
+		Test1 test = new Test1();
+		Temp temp = new Temp();
+		//자식 --> 부모 : 항상 성공
+		test = (Test1)temp;//자식이 부모로 형변환 가능
+		
+		//부모 --> 자식 : 자동형변환 실패, 강제형변환 성공 == 컴파일 || 런타임 성공or실패
+		temp = (Temp)test;
+		System.out.println(temp);
+		
+	}
+}
+class Test1{
+	
+}
+class Temp extends Test1{
+	
+}
