be5d1f15cba5e110e7231467578f6539b182057b
tmznf963 <qhfhd963@naver.com>
2018-08-24 13:41:00 +0900

    update

--- 0824/src/JdbcDemo.java
+++ 0824/src/JdbcDemo.java
@@ -1,4 +1,13 @@
+import java.sql.SQLException;
+
+import kr.co.sist.model.SeongjeokDAO;
 
 public class JdbcDemo {
-	
+	public static void main(String[] args) {
+		try {
+			SeongjeokDAO.update();
+		}catch(SQLException e) {
+			System.out.println(e);
+		}
+	}
 }
--- 0824/src/kr/co/sist/model/SeongjeokDAO.java
+++ 0824/src/kr/co/sist/model/SeongjeokDAO.java
@@ -1,5 +1,45 @@
 package kr.co.sist.model;
 
-public class SeongjeokDAO {
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class SeongjeokDAO {
+	public static void update() throws SQLException{
+		Connection conn = DBConnection.getInstance();
+		Statement stmt = conn.createStatement();
+		String [] array = {"1103","1104","1105","1106"};
+		String sql = null;
+		ResultSet rs = null;
+			for(int i = 0 ; i < array.length ; i ++) {
+				sql = "SELECT kor, eng, mat , edp  "
+								  + "FROM Student  "
+								  + "WHERE hakbun = '"+array[i]+"'  ";
+				rs = stmt.executeQuery(sql);
+				rs.next();//DB 한 줄 읽기
+				int kor = rs.getInt("kor");
+				int eng = rs.getInt("eng");
+				int mat = rs.getInt(3);//index는 1부터 시작
+				int edp = rs.getInt(4);
+				int sum = getSum(kor, eng, mat, edp);//static은 static 끼리
+				double avg = getAvg(sum);
+				sql = "UPDATE Student  "
+					  + "SET sum ="+sum+", avg ="+avg+"  "
+					  + "WHERE hakbun = '"+array[i]+"' ";
+				stmt.executeUpdate(sql);
+				System.out.println("Update Success");
+			}
+			
+		if(rs != null) rs.close();
+		if(stmt != null) stmt.close();
+		
+		DBClose.close(conn);
+	}
+	private static double getAvg(int sum) {
+		return sum / 4. ;
+	}
+	private static int getSum(int kor, int eng , int mat, int edp) {
+		return kor + eng + mat + edp;
+	}
 }
